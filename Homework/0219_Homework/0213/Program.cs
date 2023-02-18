c036e167c4e0730e4932a14833ff7900d1c8978a
Donkeymonkey <smreo0915@gmail.com>
2023-02-18 14:51:29 +0900

    0218 숙제한것 + 파일이름 바꿈

---
+++ Homework/0219_Homework/0213/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace _0213
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Zodiac zodiac = new Zodiac();
+			zodiac.Return(zodiac.BirthYear());
+			Console.WriteLine();
+			Calculator calculator = new Calculator();
+			calculator.cal();
+		}
+	}
+
+	class Zodiac
+	{
+		public string[] Animal = new string[12]
+			{"원숭이", "닭", "개", "돼지", "쥐", "소", "호랑이", "토끼", "용", "뱀", "말", "양"};
+
+		public int BirthYear()
+		{
+			Console.Write("출생년도를 입력해주세요 : ");
+			int birthYear = Convert.ToInt32(Console.ReadLine());
+
+			return birthYear;
+		}
+
+		public void Return(int birthYear)
+		{
+			int animal = birthYear % 12;
+			int age = 2023 - birthYear + 1;
+			Console.WriteLine($"당신은 {birthYear}년생 {age}세 {Animal[animal]}띠 입니다.");
+		}
+	}
+	class Calculator
+	{
+		public void cal()
+		{
+			Console.WriteLine("숫자 두 개를 입력해주세요 : ");
+			int A = Convert.ToInt32(Console.ReadLine());
+			int B = Convert.ToInt32(Console.ReadLine());
+			Console.Write("연산기호를 입력해주세요 : ");
+			string oper = Console.ReadLine();
+			int Re;
+
+			switch (oper)
+			{
+				case "+":
+					Re = A + B;
+					Console.WriteLine($"두 수의 합은 {Re}");
+					break;
+				case "-":
+					Re = A - B;
+					Console.WriteLine($"두 수의 차는 {Re}");
+					break;
+				case "*":
+					Re = A * B;
+					Console.WriteLine($"두 수의 곱은 {Re}");
+					break;
+				case "/":
+					Re = A / B;
+					Console.WriteLine($"두 수의 나누기는 {Re}");
+					break;
+				case "%":
+					Re = A % B;
+					Console.WriteLine($"두 수의 나머지는 {Re}");
+					break;
+			}
+		}
+	}
+
+}
---
+++ Homework/0219_Homework/0215/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel.Design;
+
+namespace _0215
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("1.한식  2.중식  3.양식");
+			Console.Write("식당의 번호를 입력해주세요 : ");
+			int Num = Convert.ToInt32(Console.ReadLine());
+
+			switch (Num)
+			{
+				case 1:
+					Korean korean = new Korean();
+					korean.Menu();
+					break;
+				case 2:
+					Chinese chinese = new Chinese();
+					chinese.Menu();
+					break;
+				case 3:
+					Western western = new Western();
+					western.Menu();
+					break;
+			}
+		}
+	}
+
+	class Restaurant
+	{
+		protected string[] menus;
+		protected int[] price;
+
+		protected void TakeMoney(int money)
+		{
+			Console.WriteLine($"가격은 {price[money]} 원 입니다.");
+		}
+		protected void Order(int num)
+		{
+			Console.WriteLine($"받은 주문 : {menus[num]}");
+			TakeMoney(num);
+		}
+		protected void ShowMenu()
+		{
+			Console.Write("메뉴 : ");
+			for (int i = 0; i < menus.Length; i++)
+			{
+				Console.Write($"{ menus[i]}  ");
+			}
+			Console.WriteLine();
+			Console.Write("음식의 번호를 골라주세요 : ");
+			int Choice = Convert.ToInt32(Console.ReadLine()) - 1;
+			Order(Choice);
+		}
+	}
+	class Korean : Restaurant
+	{
+		public void Menu()
+		{
+			menus = new string[3] { "비빔밥", "갈비탕", "불고기" };
+			price = new int[3] { 6000, 8000, 7000 };
+			ShowMenu();
+		}
+	}
+	class Chinese : Restaurant
+	{
+		public void Menu()
+		{
+			menus = new string[2] { "짜장면", "짬뽕" };
+			price = new int[2] { 6000, 7000 };
+			ShowMenu();
+		}
+	}
+	class Western : Restaurant
+	{
+		public void Menu()
+		{
+			menus = new string[3] { "피자", "햄버거", "파스타" };
+			price = new int[3] { 9000, 5000, 6500 };
+			ShowMenu();
+		}
+	}
+}
